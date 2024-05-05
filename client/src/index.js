33efe959ade00bd4933443436aae9d25187d828f
Beilan Guo <gbl0083@yahoo.com>
2024-05-05 21:25:19 +0200

    Solve confilcs from the previous merge from Fred's branch

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App"; // Adjust based on your file naming
@@ -14,16 +13,3 @@ if (rootElement) {
 } else {
     console.error("Failed to find the root element");
 }
-=======
-import React from 'react';
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from 'react-router-dom';
-import App from './components/App1'
-
-ReactDOM.render(
-    <Router>
-        <App />
-    </Router>,
-    document.getElementById('root')
-)
->>>>>>> freds-branch

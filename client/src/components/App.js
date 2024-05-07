71d2ad23c39df00408c1aa3013415eb1d06be305
Beilan Guo <gbl0083@yahoo.com>
2024-05-07 20:14:38 +0200

    Add signup page as well as the signup page style, modify header and add AuthContext to handle login

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,6 +3,8 @@ import Layout from './Layout';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import GameListsPage from '../pages/GameListsPage';
 import GameDetailPage from '../pages/GameDetailPage';
+import { AuthProvider } from '../context/AuthContext';
+import Signup from '../pages/SignUp';
 
 
 
@@ -10,12 +12,15 @@ const App = () => {
     return (
         <div>
             <Router>
-                <Layout>
-                    <Routes>
-                        <Route path="/" element= { <GameListsPage /> } />
-                        <Route path="/games/:gameId" element= { <GameDetailPage />} />
-                    </Routes>
-                </Layout>
+                <AuthProvider>
+                    <Layout>
+                        <Routes>
+                            <Route path="/" element= { <GameListsPage /> } />
+                            <Route path="/games/:gameId" element= { <GameDetailPage />} />
+                            <Route path="/signup" element= { <Signup />} />
+                        </Routes>
+                    </Layout>
+                    </AuthProvider>  
             </Router>
         </div>
     );

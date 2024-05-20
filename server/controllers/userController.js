8fe5d2bf236336943dc5d727f8122bf6d15a5492
LucasL <lucas.lofqvist0005@stud.hkr.se>
2024-05-20 13:50:21 +0200

    Implemented endpoint for toggeling suspensions and modified jwt authenticate function

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -56,3 +56,35 @@ export const addUser = async (req, res) => {
         res.status(500).json({success: false, message: "Something went wrong!", error: error.message});
     }
 };
+
+export const toggleUserSuspension = async (req, res) => {
+    try {
+        const { targetUser } = req.body;
+
+        if (!targetUser) {
+            return res.status(400).json({message: "Missing required field in the request body."})
+        }
+
+        const user = await User.findOne({username: targetUser});
+
+        if (!user) {
+            return res.status(404).json({message: `No user with username ${targetUser} was found.`})
+        }
+
+        let newRole;
+        if (user.role === "User") {
+            newRole = "Banned";
+        }
+        else if (user.role === "Banned") {
+            newRole = "User";
+        }
+
+        user.role = newRole;
+
+        await user.save();
+
+        res.status(200).json({message: `User suspension toggled, new role: ${newRole}`});
+    } catch (error) {
+        res.status(500).json({message: "Something went wrong!", error: error.message});
+    }
+}

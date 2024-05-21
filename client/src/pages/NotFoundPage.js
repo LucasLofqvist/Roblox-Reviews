23bb79a3e3ed93e6585aba7eb84069c70ce01656
Hiimfred <Fredrik.ottenfelt0006@stud.hkr.se>
2024-05-21 20:09:48 +0200

    small fix

--- client/src/pages/NotFoundPage.js
+++ client/src/pages/NotFoundPage.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import notFoundImage from '../img/notFound.jpg';
+// import notFoundImage from '../img/ghost.jpg';
 import '../style/notFoundPage.css';
 
 const NotFoundPage = () => {
     return (
         <div className="not-found-page">
-            <img src={notFoundImage} alt="404 Not Found" className="not-found-image" />
+            <img src="placehold.co/200x200" alt="404 Not Found" className="not-found-image" />
             <h1>Oops! Page Not Found</h1>
             <p>The game you're looking for doesn't exist.</p>
-            <Link to="/">👉Go Back Home</Link>
         </div>
     );
 };
